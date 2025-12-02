225f2a8c278b9644dbf6b9e59e9ccb7f7208986d
pritampanda15 <pritampkp15@gmail.com>
2025-12-02 11:48:23 -0800

    Add GitHub Actions workflows and update version to 3.0.0
    - Add PyPI publishing workflow (publish-pypi.yml)
      * Publishes to Test PyPI first, then PyPI
      * Triggered on release creation
    
    - Add Python package testing workflow (python-package.yml)
      * Tests on Python 3.9, 3.10, 3.11, 3.12
      * Uses conda for scientific dependencies
      * Runs flake8 linting and pytest
      * Triggered on push/PR to latest-v3.0 branch
    
    - Update version from 1.0.0 to 3.0.0 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pandadock",
-    version="1.0.0",
+    version="3.0.0",
     packages=find_packages(),
     install_requires=[
         "click>=8.0.0",
