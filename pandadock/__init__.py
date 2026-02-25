10f7f5e56ddca2ad266aeb5c223049c9cde3ca7c
pritampanda15 <pritampkp15@gmail.com>
2026-02-24 17:25:04 -0800

    updated version 4.0.2

--- pandadock/__init__.py
+++ pandadock/__init__.py
@@ -2,5 +2,5 @@
 PandaDock: GPU-accelerated molecular docking software with SE(3)-equivariant GNN scoring
 """
 
-__version__ = "4.0.1"
+__version__ = "4.0.2"
 __author__ = "Pritam Kumar Panda @Stanford University"
